8048763027b08eae5ab1b719e93adb09536c5ed6
namkoun <skarud0328@gmail.com>
2022-01-19 11:27:54 +0900

    3

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './About.css';
+
+function About() {
+    return(
+        <div className="about__container">
+            <span>About this page: I built it because I love movies.</span>
+            <span>19dfasdf15as</span>
+        </div>
+    )
+}
+
+export default About;
